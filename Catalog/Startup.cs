527ec6fa3347e96c1d921acf8c91b49864d33459
Dyako Baram <dyako.baram@gmail.com>
2021-06-25 17:26:07 +0300

    updateing dockerfile

--- Catalog/Startup.cs
+++ Catalog/Startup.cs
@@ -65,11 +65,12 @@ namespace Catalog
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog v1"));
             }
-
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog v1"));
+            if (env.IsDevelopment()){
+                app.UseHttpsRedirection();
+            }
 
             app.UseRouting();
 
